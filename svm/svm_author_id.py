00037d623a56ca236f0c126e8bc01dd4652a610f
aaghi <aditya.aghi@gmail.com>
2014-11-16 12:58:28 -0500

    adding solutions to nb, decision tree, svm

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -20,7 +20,8 @@ from email_preprocess import preprocess
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
-
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100] 
 
 
 
@@ -28,5 +29,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 
 #########################################################
-
+from sklearn.svm import SVC
+import numpy
+clf = SVC(kernel="rbf",C=10000.0)
+t0 = time()
+clf.fit(features_train,labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t1 = time()
+pred = clf.predict(features_test)
+print "10 th "+str(pred[10])
+print "26 th "+str(pred[26])
+print "50 th "+str(pred[50])
+bins = numpy.bincount(pred)
+print "number of emails from chris "+str(bins[1])
+print "prediction time:", round(time()-t1, 3), "s"
+from sklearn.metrics import accuracy_score
+print accuracy_score(labels_test,pred)
 
